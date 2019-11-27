bc9254244b665b9928c3168f64e346b14e05351f
James Eneh <erozonachi.james@gmail.com>
2019-11-27 08:29:40 +0100

    Remove redundant codes

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <!-- <logo /> -->
       <img :src="Avatar" alt="Eneh's Display Pix" class="avatar" />
       <h1 class="title">James, Eneh</h1>
-      <!--
-      <h2 class="subtitle">
-        James Eneh&#39;s portfolio project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>-->
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import Avatar from '~/assets/avatar.png'
 
 export default {
   components: {
-    Logo
+    /* Logo */
   },
   data: () => ({
     Avatar
