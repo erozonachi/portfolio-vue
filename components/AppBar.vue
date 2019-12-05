4d348d122d11c4e25372ff5ff109dfd759dd834c
James Eneh <erozonachi.james@gmail.com>
2019-12-05 22:51:22 +0100

    Add template to appbar component

---
+++ components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar">
+    <div class="container">
+      <div class="user-info">
+        <img :src="Avatar" alt="Eneh's Display Pix" class="avatar" />
+        <h1 class="title">James, Eneh</h1>
+      </div>
+      <div class="social-handles">
+        <a target="_blank_" href="#"><span></span></a>
+        <a target="_blank_" href="#"><span></span></a>
+        <a target="_blank_" href="#"><span></span></a>
+      </div>
+    </div>
+  </div>
+</template>
