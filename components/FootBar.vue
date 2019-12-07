517b240614fe083e062c263cd0ddef3a1fe11de1
James Eneh <erozonachi.james@gmail.com>
2019-12-07 11:25:18 +0100

    Add template to footbar

---
+++ components/FootBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footbar">
+    <div class="container">
+      <button class="commands">···</button>
+      <form>
+        <input type="text" />
+        <button type="submit">send</button>
+      </form>
+    </div>
+  </div>
+</template>
