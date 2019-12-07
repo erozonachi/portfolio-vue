70a0b3adc882e75374d5fac441460e1c21e83482
James Eneh <erozonachi.james@gmail.com>
2019-12-07 09:34:39 +0100

    Add navbar styling

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -13,3 +13,18 @@
     </div>
   </div>
 </template>
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  height: 50px;
+  background-color: #ffffff;
+  border: none;
+  border-bottom: 1px solid #f0f0f0;
+  color: #4f4f4f;
+}
+</style>
