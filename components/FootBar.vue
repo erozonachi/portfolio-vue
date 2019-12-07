f4c68c7a9f9e7f4655c14459855eb3bb5eca4e49
James Eneh <erozonachi.james@gmail.com>
2019-12-07 11:48:21 +0100

    Add form, input and button styling

--- components/FootBar.vue
+++ components/FootBar.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <button class="commands">···</button>
       <form>
-        <input type="text" />
+        <input type="text" placeholder="Type message..." />
         <button type="submit">send</button>
       </form>
     </div>
@@ -36,5 +36,34 @@
   background-color: inherit;
   font-weight: bold;
   font-size: 1.6rem;
+  color: #4265ed;
+}
+form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+}
+input,
+button {
+  outline: none;
+  border: none;
+  padding: 1rem;
+}
+input {
+  border-radius: 1rem;
+  font-size: 1.8rem;
+  margin: 30px 0;
+  background-color: #e4f0f7;
+  width: 90%;
+}
+button {
+  border-radius: 2rem;
+  font-size: 1.8rem;
+  background-color: inherit;
+  color: #4265ed;
+  cursor: pointer;
+  text-transform: capitalize;
+  font-weight: bold;
 }
 </style>
