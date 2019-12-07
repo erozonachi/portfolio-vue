b3c86866f06dffe2ad8223df479d67d1d381552c
James Eneh <erozonachi.james@gmail.com>
2019-12-07 12:32:19 +0100

    Add chats template and styling

---
+++ components/Chats.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chats"></div>
+</template>
+<style scoped>
+.chats {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  height: 100vh;
+  overflow-y: scroll;
+  max-width: 800px;
+  width: 100%;
+  background-color: #f4f4f4;
+  padding-top: 50px;
+  padding-bottom: 60px;
+}
+</style>
