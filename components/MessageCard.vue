a62f2ef33ebc2d6a835bb4f61b5fefa5b605c9a4
James Eneh <erozonachi.james@gmail.com>
2019-12-07 22:22:16 +0100

    Add style to message-card component

--- components/MessageCard.vue
+++ components/MessageCard.vue
@@ -3,3 +3,25 @@
     <p>1. My name is James, Eneh; you're lookiing at my portfolio app...</p>
   </div>
 </template>
+<style scoped>
+.sent,
+.received {
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  width: fit-content;
+  border-radius: 5px;
+  border: 1px solid #ffffff;
+  padding: 10px;
+  margin: 10px;
+  color: #333333;
+}
+.sent {
+  background-color: #e4f0f7;
+  align-self: flex-end;
+}
+.received {
+  background-color: #fafafa;
+  align-self: flex-start;
+}
+</style>
