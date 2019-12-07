c74be2bd914db78c83178f53179468f4c6c3cbea
James Eneh <erozonachi.james@gmail.com>
2019-12-07 11:26:14 +0100

    Add footbar and container styling

--- components/FootBar.vue
+++ components/FootBar.vue
@@ -9,3 +9,25 @@
     </div>
   </div>
 </template>
+<style scoped>
+.footbar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  height: 60px;
+  background-color: #ffffff;
+  border: none;
+  border-top: 1px solid #f0f0f0;
+  color: #4f4f4f;
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 800px;
+  width: 100%;
+}
+</style>
