46d2825b3e94c1e7e28b92fde45ed859c4827728
James Eneh <erozonachi.james@gmail.com>
2019-12-09 00:03:08 +0100

    Add and use stack and send message methods

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <StartScreen v-bind:startChatSession="startChatSession" v-if="!username" />
   <div v-else>
     <AppBar />
-    <Chats :username="username" />
-    <FootBar />
+    <Chats :messages="messages" />
+    <FootBar :sendMessage="sendMessage" />
   </div>
 </template>
 
@@ -21,11 +21,71 @@ export default {
     Chats
   },
   data: () => ({
-    username: ''
+    username: '',
+    messages: [
+      {
+        id: 1,
+        type: `sent`
+      },
+      {
+        id: 2,
+        type: `received`
+      },
+      {
+        id: 3,
+        type: `sent`
+      },
+      {
+        id: 4,
+        type: `received`
+      },
+      {
+        id: 5,
+        type: `sent`
+      },
+      {
+        id: 6,
+        type: `received`
+      },
+      {
+        id: 7,
+        type: `sent`
+      },
+      {
+        id: 8,
+        type: `received`
+      },
+      {
+        id: 9,
+        type: `sent`
+      },
+      {
+        id: 10,
+        type: `received`
+      },
+      {
+        id: 11,
+        type: `sent`
+      },
+      {
+        id: 12,
+        type: `received`
+      }
+    ]
   }),
   methods: {
     startChatSession: function(username) {
       this.username = username
+    },
+    stackMessage: function(message) {
+      this.messages.unshift(message)
+    },
+    sendMessage: function(message) {
+      this.stackMessage({
+        id: this.messages.length + 1,
+        type: 'sent',
+        text: message
+      })
     }
   }
 }
